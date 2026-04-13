40a0d605ac8b0e882522038f38be1e1b6fdc5f14
njmdev03 <njmdev03@gmail.com>
2026-04-13 12:23:35 -0500

    Remove old tests

--- tests/ml_runner/test_exporter.py
+++
@@ -1,6 +0,0 @@
-from ml_runner.core.registries.exporter import ExporterRegistry
-
-
-def test_data_exporter_registered():
-    assert 'data' in ExporterRegistry.all()
-    assert 'plot' in ExporterRegistry.all()
